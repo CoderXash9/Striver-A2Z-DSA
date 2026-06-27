1f9702354275e559ca48703dfad058d2ae3a3403
CoderXash9 <asahwiniprohit@gmail.com>
2026-06-27 18:15:17 +0530

    Solve Search in 2D matrix II

---
+++ BS  2D array/Search_in_2d_matrix_II.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        n, m = len(matrix), len(matrix[0])
+        rows = 0
+        cols = m - 1
+        while rows < n and cols >= 0:
+            if matrix[rows][cols] == target:
+                return True
+            elif matrix[rows][cols] < target:
+                rows += 1
+            else:
+                cols = cols - 1
+        return False
