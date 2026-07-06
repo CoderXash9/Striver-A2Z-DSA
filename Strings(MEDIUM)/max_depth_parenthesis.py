d5f0a49a4e095d5463fe079eb115ee7930dffa96
CoderXash9 <asahwiniprohit@gmail.com>
2026-07-06 16:39:26 +0530

    solved maximum depth of parenthesis question

---
+++ Strings(MEDIUM)/max_depth_parenthesis.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        curr_depth = 0
+        max_depth = 0
+
+        for brac in s:
+            if brac == "(":
+                curr_depth += 1
+                max_depth = max(curr_depth, max_depth)
+            elif brac == ")":
+                curr_depth -= 1
+
+        return max_depth
